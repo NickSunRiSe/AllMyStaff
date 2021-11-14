f431f0b735b7bf084c7f5f402d7821e4dfc2a8b6
Nick <bbbb.bbbb.2001.nik@gmail.com>
2021-11-14 17:51:23 +0400

    Слайдер готов, доделать анимацию покрасивше и формы чутка
    анимация, кнопки, работают

--- java.js
+++ java.js
@@ -1,6 +1,3 @@
-
-
-
 let audio = document.getElementById("audio");
 let tits = document.getElementById("tits");
 let time = document.querySelector(".time");
@@ -62,7 +59,7 @@ btnPlayvid.addEventListener("click", function () {
 btnPausevid.addEventListener("click", function () {
   video.pause();
 });
-window.alert("Анимация слайдер, доделать плеер");
+window.alert("Докрасить слайдер, доделать плеер");
 window.onload = function () {
   document.body.classList.add("loaded_hiding");
   window.setTimeout(function () {
@@ -70,3 +67,35 @@ window.onload = function () {
     document.body.classList.remove("loaded_hiding");
   }, 500);
 };
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function nextSlide() {
+  showSlides((slideIndex += 1));
+}
+
+function previousSlide() {
+  showSlides((slideIndex -= 1));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("item");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let slide of slides) {
+    slide.style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
