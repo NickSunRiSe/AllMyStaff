5ea8820b765d623c06768587fd50535943fe48d7
Nick <bbbb.bbbb.2001.nik@gmail.com>
2021-11-24 22:45:17 +0400

    ybxtuj
    nothing

--- java.js
+++ java.js
@@ -1,22 +1,17 @@
 let audio = document.getElementById("audio");
-let tits = document.getElementById("tits");
 let time = document.querySelector(".time");
 let btnPlay = document.querySelector(".play");
 let btnPause = document.querySelector(".pause");
 let btnPrev = document.querySelector(".prev");
 let btnNext = document.querySelector(".next");
 let lp = document.querySelector(".lp");
-let track = document.getElementById("track_item");
 var playlist = ["sounds/111.mp3", "sounds/222.mp3", "sounds/333.mp3"];
-var tr = [{tr1:"1"}, {tr1: "2"}, {tr1:"3"}]
 let mp3;
-let t;
+
 
 
 window.onload = function () {
   mp3 = 0;
-  t = 0;
-  showTrack(t);
 };
 btnPlay.addEventListener("click", function () {
   audio.play();
@@ -27,16 +22,10 @@ btnPlay.addEventListener("click", function () {
     if (audioTime == audioLength && mp3 < 2) {
       //колличество треков ручное, длинна не срабатывает
       mp3++;
-      t++;
-      track.src = tr[tr1]
       audio.src = playlist[mp3];
-      showTrack((slideIndex = n))
       audio.play();
     } else if (audioTime == audioLength) {
       mp3 = 0;
-      t=0;
-      track.src = tr[tr1]
-      showTrack((slideIndex = n))
       audio.src = playlist[mp3];
       audio.play();
     }
@@ -46,18 +35,16 @@ btnPause.addEventListener("click", function () {
   audio.pause();
   clearInterval(audioPlay);
 });
-btnPrev.addEventListener("click", function () {
-  audio.pause();
+function prevtrack(){
   if (mp3 > 0) {
     mp3--;
   } else {
-    mp3 = 3;
+    mp3 = 2;
   }
   audio.src = playlist[mp3];
   audio.play();
-});
-btnNext.addEventListener("click", function () {
-  audio.pause();
+};
+function nexttrack() {
   if (mp3 < 2) {
     mp3++;
   } else {
@@ -65,7 +52,7 @@ btnNext.addEventListener("click", function () {
   }
   audio.src = playlist[mp3];
   audio.play();
-});
+};
 
 let video = document.getElementById("video");
 let btnPlayvid = document.querySelector(".playv");
