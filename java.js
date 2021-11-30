ee286945c621cb64230275f835553cb34dedcd52
Nick <bbbb.bbbb.2001.nik@gmail.com>
2021-12-01 01:51:06 +0400

    Полностью пофиксил плеер
    нужно добавить названия треков

--- java.js
+++ java.js
@@ -3,47 +3,42 @@ let btnPlay = document.querySelector(".play");
 let btnPause = document.querySelector(".pause");
 let btnPrev = document.querySelector(".prev");
 let btnNext = document.querySelector(".next");
-let lp = document.querySelector(".lp");
 let track = document.getElementById("track_item");
-var playlist = ["sounds/111.mp3", "sounds/222.mp3", "sounds/333.mp3"];
-let songNum;
-window.onload = function () {
-  songNum = 0;
-  };
-  btnPlay.addEventListener("click", function () {
+var playlist = ["sounds/111.mp3" , "sounds/222.mp3" , "sounds/333.mp3"];
+let songNum = 0;
+
+btnPlay.addEventListener("click", function () {
   audio.play();
   audioPlay = setInterval(function () {
-  let audioTime = Math.round(audio.currentTime);
-  let audioLength = Math.round(audio.duration);
-  time.style.width = (audioTime * 100) / audioLength + "%";
-  if (audioTime == audioLength && songNum < 2) {
-  //колличество треков ручное, длинна не срабатывает
-  songNum++;
-  audio.src = playlist[songNum];
-  audio.play();
-  } else if (audioTime == audioLength) {
-    songNum = 0;
-  audio.src = playlist[songNum];
-  audio.play();
-  }
+    let audioTime = Math.round(audio.currentTime);
+    let audioLength = Math.round(audio.duration);
+    time.style.width = (audioTime * 100) / audioLength + "%";
+    if (audioTime == audioLength && songNum < 2) {
+      //колличество треков ручное, длинна не срабатывает
+      songNum++;
+      audio.src = playlist[songNum];
+      audio.play();
+    } else if (audioTime == audioLength) {
+      songNum = 0;
+      audio.src = playlist[songNum];
+      audio.play();
+    }
   }, 10);
-  });
-  btnPause.addEventListener("click", function () {
+});
+btnPause.addEventListener("click", function () {
   audio.pause();
   clearInterval(audioPlay);
-  });
-  function nexttrack() {
-    audio.pause();
+});
+function nexttrack() {
   if (songNum < 2) {
     songNum++;
-  } else {
+  } else if (songNum == 2) {
     songNum = 0;
   }
   audio.src = playlist[songNum];
   audio.play();
-  };
-  function prevtrack(){
-    audio.pause();
+}
+function prevtrack() {
   if (songNum > 0) {
     songNum--;
   } else {
@@ -51,14 +46,7 @@ window.onload = function () {
   }
   audio.src = playlist[songNum];
   audio.play();
-  };
-  
-
-
-
-
-
-
+}
 
 let video = document.getElementById("video");
 let btnPlayvid = document.querySelector(".playv");
@@ -109,5 +97,3 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
 }
-  
-
