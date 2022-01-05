5c9860feec793b46c959e8958be7075ad1c47626
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-01-05 19:13:58 +0400

    Изучение реакта 01.05.2022

--- newstarts/src/App.js
+++ newstarts/src/App.js
@@ -1,35 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
+import '../src/index';
+import End from "./components/End";
+import Start from "./components/Start";
+import Text1 from "./components/Text1";
+
 
 function App() {
   return(
-    <div className="con">
-      <header className="up"> 
-        Истень
-        <button class="play" type="button"></button>
-        <button class="pause" type="button"></button>
-        <button class="prev" type="button"></button>
-        <button class="next" type="button"></button>
-        <div class="audio-track"></div>
-        <div class="time"></div>
-        </header>
-       <div class="left">
-        <p>
-          Это сайт посвещён творчеству исполнителя Истень <br />
-          Моё имя Никита Рассветов и я Истень! <br />
-          Тут находится вся основная информация про мою группу,<br />
-          а также полная дискография со всеми песнями вышедшими на площадки<br />
-        </p>
-      </div>
-      <footer class="end">
-      <hr />
-      <p>
-        2022 <br />
-        Завхоз Инкорпарейтед <br />
-        Сратов, РА СИ Я. <br />
-      </p>
-    </footer>
+     <div className="con">
+      <Start/>
+      <Text1/>
+      <End/>
+     </div>
     
-    </div>
   );
 }
 
