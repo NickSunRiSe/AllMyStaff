3723cb7d77acae1709b43374852eb98622fb0fc7
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-01-07 19:48:35 +0400

    Изучение реакта 01.07.2022

--- newstarts/src/components/Text2.jsx
+++ newstarts/src/components/Text2.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 const Text2 = (props) => {
+    
     return (
+        
         <div className='text2'>
             <div className='post__content'>
             {props.post.id} . {props.post.title}
             </div>
             {props.post.body}
+            {props.post.picture}
         </div>
     );
 };
