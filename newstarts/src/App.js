e80fd30423dfcf33a0aa2bf09dd56cd8958157a2
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-01-08 20:22:35 +0400

    Изучение реакта 01.08.2022

--- newstarts/src/App.js
+++ newstarts/src/App.js
@@ -1,16 +1,19 @@
 import React, {useState} from "react";
 import '../src/index';
 import End from "./components/End";
+import Foto from "./components/Foto";
 import Start from "./components/Start";
 import Text1 from "./components/Text1";
 import Text2 from "./components/Text2";
+
+
 
 
 function App() {
   const [posts, setPosts] = useState ([
     {id: 1, title:'Сборник 1: Сломанный телефон', body:'Первый полноценный альбом, который является по сути всего лишь сборником вышедших на тот момент песен'},
     {id: 2, title:'Причины искусства', body:'Второй полноформатный альбом вышедший через полтора года после первого и также вместивший много песен вышедших ранее'},
-    {id: 3, title:'Грустные песни о успехах моей жизни', body:'Альбом вышедший в начале 2022 года'},
+    {id: 3, title:'Грустные песни о успехах моей жизни', body:'Альбом вышедший в начале 2022 года', picture: <Foto/> },
   ])
 
 
@@ -24,6 +27,7 @@ function App() {
       <Text2 post = {post} key={post.id}/>
 
       )}
+     
       <End/>
      </div>
     
---
+++ newstarts/src/components/Foto.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Fotochka from '../public/Fotochka.jpg'
+
+const Foto = () => {
+    return (
+        <img src={Fotochka} className='foto'></img>
+    );
+};
+
+export default Foto;
