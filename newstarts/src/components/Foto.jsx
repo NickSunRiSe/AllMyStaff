097d11a579d1d52793575aac0dd62364ac591862
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-01-09 22:06:38 +0400

    Изучение реакта 01.09.2022

--- newstarts/src/components/Foto.jsx
+++ newstarts/src/components/Foto.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import Fotochka from '../public/Fotochka.jpg'
-
+import Fotochka from './Fotochka.jpg'
 const Foto = () => {
     return (
         <img src={Fotochka} className='foto'></img>
