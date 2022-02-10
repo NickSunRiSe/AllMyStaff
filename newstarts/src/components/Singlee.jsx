6c1ff4ce12e43123f51c31adec8953d084218a6b
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-02-10 20:00:13 +0400

    Изучение реакта 02.10.2022

--- newstarts/src/components/Singlee.jsx
+++ newstarts/src/components/Singlee.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
-import Apple from "./svg/Apple";
-import Spotify from "./svg/SpotL";
-import Vk from "./svg/Vk";
-import Yan from "./svg/Yan";
 import classes from "./Text2.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faSpotify,
+  faVk,
+  faYandex,
+  faApple
+} from '@fortawesome/free-brands-svg-icons';
 
 const Singles = (props) => {
+  library.add(
+    faSpotify,
+    faVk,
+    faYandex,
+    faApple
+  );
     return (
       <div className={classes.teht}>
           {props.single.id} . {props.single.title} <br/>
         {props.single.body}
         <div className="logosfoto">
         <div className="logos">
-          <a href={props.single.spotify} target={"_blank"} className="links">
-            <Spotify/>
-          </a>
-          <a href={props.single.Vk} target={"_blank"} className="links">
-            <Vk/>
-          </a>
-          <a href={props.single.Yandex} target={"_blank"} className="links">
-            <Yan/>
-          </a>
-          <a href={props.single.Apple} target={"_blank"} className="links">
-            <Apple/>
-          </a>
+        <a href={props.single.spotify} className="links" target="_blank" rel="noopener noreferrer">
+  <FontAwesomeIcon icon={faSpotify} size='4x' />
+</a>
+<a href={props.single.Vk} className="links" target="_blank" rel="noopener noreferrer">
+  <FontAwesomeIcon icon={faVk} size='4x' />
+</a>
+<a href={props.single.Yandex} className="links" target="_blank" rel="noopener noreferrer">
+  <FontAwesomeIcon icon={faYandex} size='4x' />
+</a>
+<a href={props.single.Apple} className="links" target="_blank" rel="noopener noreferrer">
+  <FontAwesomeIcon icon={faApple} size='4x' />
+</a>
           </div>
       {props.single.picture}
       </div>
--- newstarts/src/components/Text2.jsx
+++ newstarts/src/components/Text2.jsx
@@ -1,29 +1,40 @@
 import React from "react";
-import Apple from "./svg/Apple";
-import Spotify from "./svg/SpotL";
-import Vk from "./svg/Vk";
-import Yan from "./svg/Yan";
 import classes from "./Text2.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faSpotify,
+  faVk,
+  faYandex,
+  faApple
+} from '@fortawesome/free-brands-svg-icons';
 
 const Text2 = (props) => {
+  library.add(
+    faSpotify,
+    faVk,
+    faYandex,
+    faApple
+  );
+  
   return (
     <div className={classes.teht}>
       {props.post.id} . {props.post.title} <br />
       {props.post.body}
       <div className="logosfoto">
         <div className="logos">
-          <a href={props.post.spotify} target={"_blank"} className="links">
-            <Spotify />
-          </a>
-          <a href={props.post.Vk} target={"_blank"} className="links">
-            <Vk />
-          </a>
-          <a href={props.post.Yandex} target={"_blank"} className="links">
-            <Yan />
-          </a>
-          <a href={props.post.Apple} target={"_blank"} className="links">
-            <Apple />
-          </a>
+        <a href={props.post.spotify} className="links" target="_blank" rel="noopener noreferrer">
+  <FontAwesomeIcon icon={faSpotify} size='4x' />
+</a>
+<a href={props.post.Vk} className="links" target="_blank" rel="noopener noreferrer">
+  <FontAwesomeIcon icon={faVk} size='4x' />
+</a>
+<a href={props.post.Yandex} className="links" target="_blank" rel="noopener noreferrer">
+  <FontAwesomeIcon icon={faYandex} size='4x' />
+</a>
+<a href={props.post.Apple} className="links" target="_blank" rel="noopener noreferrer">
+  <FontAwesomeIcon icon={faApple} size='4x' />
+</a>
         </div>
         {props.post.picture}
       </div>
