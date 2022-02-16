5522aaf279feea714556813c9db2932886e4246b
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-02-16 16:49:01 +0400

    Изучение реакта 02.16.2022

--- newstarts/src/App.js
+++ newstarts/src/App.js
@@ -1,22 +1,10 @@
 import React, { useState } from "react";
-import { BrowserRouter, Link, Route } from "react-router-dom";
 import "../src/index";
-import Navbar from "./components/UI/Navbar";
 import Allinfo from "./pages/Allinfo";
-import FP from "./pages/Fs";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Navbar/>
-    <Route path="/123">
-      <Allinfo/>
-    </Route>
-    <Route path="/321">
-      <FP/>
-    </Route>
-    </BrowserRouter>
-  
+          <Allinfo />
   );
 }
 
