79b2bf16df2f73f42ca6f27ac8eafaf90bf2dc70
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-02-26 02:04:08 +0400

    Изучение реакта 02.26.2022

--- newstarts/src/components/UI/Header.jsx
+++ newstarts/src/components/UI/Header.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Link, Route } from "react-router-dom";
 const Header = () => {
     return (
         <header className="header">
-        Это верхушка страницы - header тут меню
+        Это верхушка страницы - header тут меню страниц которое я не сделал
       </header>
     );
 };
