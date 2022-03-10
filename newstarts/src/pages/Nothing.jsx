ff14a431bc7f2b5fe29eb17ef310257c56d5cc57
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-03-10 17:12:23 +0400

    Изучение реакта 03.10.2022

--- newstarts/src/pages/Nothing.jsx
+++ newstarts/src/pages/Nothing.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Header from '../components/UI/Header';
 import LogoMain from "../components/images/LogoMain"
+import End from "../components/End"
 
 const Nothing = () => {
     return (
-        
         <div>
           <div className="LogoMainDiv">
         <LogoMain />
@@ -13,7 +13,7 @@ const Nothing = () => {
       <div className='nothing'>
       Данная страница не содержит какого либо контента.
       </div>
-      
+      <End/>
     </div>
     );
 };
