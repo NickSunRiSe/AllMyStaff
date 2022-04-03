9f13373f9d9819d63b7bb2ff34a932c4e32321e2
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-04-03 11:38:57 +0400

    Изучение реакта 04.03.2022

--- newstart2/src/pages/Contacts.jsx
+++ newstart2/src/pages/Contacts.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import End from '../End';
-import Header from '../Header';
+import React from "react";
+import End from "../End";
+import Header from "../Header";
 
 const Contacts = () => {
-    return (
-        <div className='con'>
-            <Header/>
-            Контакты
-            <End/>
-        </div>
-    );
+  return (
+    <div className="con">
+      <Header />
+      <h1>Контакты</h1>
+      По вопросам сотрудничества и предложений пишите и обращайтесь на почту.
+      <br />
+      <div className="ContactsLine">
+      <div className="contact1">
+        Все коммерческие предложения <br />
+        istenoff@gmail.com <br />
+        Рассветов Никита <br />
+      </div>
+      <div className="contact1">
+        Обращения с дополнительными вопросами <br />
+        istenoff@gmail.com <br />
+        Рассветов Никита <br />
+      </div>
+      <div className="contact1">
+        Обращения напрямую к Истень <br />
+        https://vk.com/nicksunrise <br />
+        Рассветов Никита <br />
+      </div>
+      </div>
+      <End />
+    </div>
+  );
 };
 
 export default Contacts;
