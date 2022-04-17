b3d8544e8f0831c20a23cc159e04db34c4a9d88b
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-04-17 15:14:32 +0400

    Изучение реакта 04.17.2022

--- newstart2/src/components/JS/Player.jsx
+++ newstart2/src/components/JS/Player.jsx
@@ -1,60 +1,88 @@
-import React, { useRef, useState } from "react";
-import Ustal from "../sounds/Ustal.mp3";
+import React from 'react';
 
-export default function App() {
-  const audioPlayer = useRef();
-  const [currentTime, setCurrentTime] = useState(0);
-  const [seekValue, setSeekValue] = useState(0);
-
-  const play = () => {
-    audioPlayer.current.play();
-  };
-  const prev = () => {
-    audioPlayer.current.prev();
-  };
-  const next = () => {
-    audioPlayer.current.next();
-  };
-  const pause = () => {
-    audioPlayer.current.pause();
-  };
-  const stop = () => {
-    audioPlayer.current.pause();
-    audioPlayer.current.currentTime = 0;
-  };
-  const onPlaying = () => {
-    setCurrentTime(audioPlayer.current.currentTime);
-    setSeekValue(
-      (audioPlayer.current.currentTime / audioPlayer.current.duration) * 100
-    );
-  };
+const Player = () => {
   return (
-    <div className="App">
-      <audio source={Ustal} ref={audioPlayer} onTimeUpdate={onPlaying}>
-        Your browser does not support the
-        <code>audio</code> element.
-      </audio>
-      <br />
-      <p>{currentTime}</p>
-      <input
-        type="range"
-        min="0"
-        max="100"
-        step="1"
-        value={seekValue}
-        onChange={(e) => {
-          const seekto = audioPlayer.current.duration * (+e.target.value / 100);
-          audioPlayer.current.currentTime = seekto;
-          setSeekValue(e.target.value);
-        }}
-      />
-      <div>
-        <button onClick={play}>&#9655;</button>
-        <button onClick={pause}>&#9723;</button>
-        <button onclick={prev}>&#8810;</button>
-        <button onclick={next}>&#8811;</button>
-        <button onClick={stop}>stop</button>
-      </div>
-    </div>
+    <div class="tabs-albums" id="tabs-1">
+                <div class="tabs-albums-select">
+                  <h6 class="text-gray-600">Выберите альбом</h6>
+                  <ul class="nav nav-tabs">
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#tabs-1-1" data-toggle="tab">TRAGIC CITY</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-1-2" data-toggle="tab">КОНДИТЕРСКАЯ</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-1-3" data-toggle="tab">MAGIC CITY</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-1-4" data-toggle="tab">ROMANTIC COLEGTION</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-1-5" data-toggle="tab">ВИСЕЛИЦЦА</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-1-6" data-toggle="tab">ЁП</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-1-7" data-toggle="tab">ВИДЕТЬ ЦВЕТНЫЕ СНЫ</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-1-8" data-toggle="tab">СВИНОЕ РЫЛО</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-1-9" data-toggle="tab">ONE MORE CITY</a></li>
+                  </ul>
+                </div>
+
+
+
+
+
+
+                <div class="tab-content">
+                  <div class="tab-pane fade show active" id="tabs-1-1">
+                    <div class="jp-modern jp-player-init jp-audio-0 jp-video-270p">
+                      <div class="jp-jplayer" id="jp_jplayer_0" style="width: 480px; height: 270px;"><audio id="jp_audio_0" preload="metadata" src="https://lspofficial.ru/discography/audio/%D0%9B%D0%A1%D0%9F---%D0%9F%D0%BE%D0%BB%D0%B7%D0%B0%D1%82%D1%8C.mp3" title="ЛСП - Ползать"></audio><video id="jp_video_0" preload="metadata" style="width: 0px; height: 0px;" title="ЛСП - Ползать"></video></div>
+                      <div class="jp-audio jp-audio-0 jp-video-270p" role="application" aria-label="media player">
+                        <ul class="jp-player-list">
+                          <li class="jp-player-list-item" data-jp-title="ЛСП - Ползать" data-jp-mp3="audio/ЛСП---Ползать.mp3"></li>
+                          <li class="jp-player-list-item" data-jp-title="ЛСП - Монетка" data-jp-mp3="audio/ЛСП---Монетка.mp3"></li>
+                          <li class="jp-player-list-item" data-jp-title="ЛСП - Деньги не проблема" data-jp-mp3="audio/ЛСП---Деньги-не-проблема.mp3"></li>
+                          <li class="jp-player-list-item" data-jp-title="ЛСП - Тело" data-jp-mp3="audio/ЛСП---Тело.mp3"></li>
+                          <li class="jp-player-list-item" data-jp-title="ЛСП - Воскресение" data-jp-mp3="audio/ЛСП---Воскресение.mp3"></li>
+                          <li class="jp-player-list-item" data-jp-title="ЛСП - Лабиринт-отражений" data-jp-mp3="audio/ЛСП---Лабиринт-отражений.mp3"></li>
+                          <li class="jp-player-list-item" data-jp-title="ЛСП - Белый танец" data-jp-mp3="audio/ЛСП---Белый-танец.mp3"></li>
+                          <li class="jp-player-list-item" data-jp-title="ЛСП - Отброс" data-jp-mp3="audio/ЛСП---Отброс.mp3"></li>
+                          <li class="jp-player-list-item" data-jp-title="ЛСП - Канкан" data-jp-mp3="audio/ЛСП---Канкан.mp3"></li>
+                          <li class="jp-player-list-item" data-jp-title="ЛСП - Ещё один день" data-jp-mp3="audio/ЛСП---Ещё-один-день.mp3"></li>
+                          <li class="jp-player-list-item" data-jp-title="ЛСП - Интерлюдия-(feat.-Лёха-Никонов)" data-jp-mp3="audio/ЛСП---Интерлюдия-(feat.-Лёха-Никонов).mp3"></li>
+                          <li class="jp-player-list-item" data-jp-title="ЛСП - Конец-света" data-jp-mp3="audio/ЛСП---Конец-света.mp3"></li>
+                          <li class="jp-player-list-item" data-jp-title="ЛСП - Плевок в вечность" data-jp-mp3="audio/ЛСП---Плевок-в-вечность.mp3"></li>
+                        </ul>
+
+
+
+
+
+
+
+                        <div class="jp-modern-interface">
+                          <div class="jp-button jp-playpaus-button">
+                            <button class="jp-btn jp-play" tabindex="0"></button>
+                          </div>
+                          <div class="jp-time-rail">
+                            <div class="jp-progress">
+                              <div class="jp-seek-bar" style="width: 100%;">
+                                <div class="jp-play-bar" style="width: 0%;"></div>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="jp-current-time">00:00</div>
+                          <div class="jp-button jp-volume-button">
+                            <button class="jp-btn jp-mute" tabindex="0">max volume</button>
+                          </div>
+                        </div>
+                        <div class="jp-playlist">
+                          <ul class="jp-playlist-inner" style=""><li class="jp-playlist-current"><div><a href="javascript:;" class="jp-playlist-item-remove" style="display: none;">×</a><a href="javascript:;" class="jp-playlist-item jp-playlist-current" tabindex="0">ЛСП - Ползать</a></div></li><li><div><a href="javascript:;" class="jp-playlist-item-remove" style="display: none;">×</a><a href="javascript:;" class="jp-playlist-item" tabindex="0">ЛСП - Монетка</a></div></li><li><div><a href="javascript:;" class="jp-playlist-item-remove" style="display: none;">×</a><a href="javascript:;" class="jp-playlist-item" tabindex="0">ЛСП - Деньги не проблема</a></div></li><li><div><a href="javascript:;" class="jp-playlist-item-remove" style="display: none;">×</a><a href="javascript:;" class="jp-playlist-item" tabindex="0">ЛСП - Тело</a></div></li><li><div><a href="javascript:;" class="jp-playlist-item-remove" style="display: none;">×</a><a href="javascript:;" class="jp-playlist-item" tabindex="0">ЛСП - Воскресение</a></div></li><li><div><a href="javascript:;" class="jp-playlist-item-remove" style="display: none;">×</a><a href="javascript:;" class="jp-playlist-item" tabindex="0">ЛСП - Лабиринт-отражений</a></div></li><li><div><a href="javascript:;" class="jp-playlist-item-remove" style="display: none;">×</a><a href="javascript:;" class="jp-playlist-item" tabindex="0">ЛСП - Белый танец</a></div></li><li><div><a href="javascript:;" class="jp-playlist-item-remove" style="display: none;">×</a><a href="javascript:;" class="jp-playlist-item" tabindex="0">ЛСП - Отброс</a></div></li><li><div><a href="javascript:;" class="jp-playlist-item-remove" style="display: none;">×</a><a href="javascript:;" class="jp-playlist-item" tabindex="0">ЛСП - Канкан</a></div></li><li><div><a href="javascript:;" class="jp-playlist-item-remove" style="display: none;">×</a><a href="javascript:;" class="jp-playlist-item" tabindex="0">ЛСП - Ещё один день</a></div></li><li><div><a href="javascript:;" class="jp-playlist-item-remove" style="display: none;">×</a><a href="javascript:;" class="jp-playlist-item" tabindex="0">ЛСП - Интерлюдия-(feat.-Лёха-Никонов)</a></div></li><li><div><a href="javascript:;" class="jp-playlist-item-remove" style="display: none;">×</a><a href="javascript:;" class="jp-playlist-item" tabindex="0">ЛСП - Конец-света</a></div></li><li><div><a href="javascript:;" class="jp-playlist-item-remove" style="display: none;">×</a><a href="javascript:;" class="jp-playlist-item" tabindex="0">ЛСП - Плевок в вечность</a></div></li></ul>
+                        </div>
+                        <div class="jp-modern-footer">
+                          <p>Понравился альбом? Оцени на площадке!</p>
+                          <ul class="jp-modern-linklist">
+                            <li><a href="https://music.apple.com/ru/artist/%D0%BB%D1%81%D0%BF/943459711"><span>iTunes</span></a></li>
+                            <li><a href="https://play.google.com/store/music/artist/%D0%9B%D0%A1%D0%9F?id=A236mhdluifjthwzdk6hskldqia&amp;hl=en_US"><span>Google</span></a></li>
+                            <li><a href="https://open.spotify.com/artist/4h8pGxEIOi7j4me1yhYxlD"><span>Spotify</span></a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+</div>
+</div>
   );
-}
+};
+
+export default Player;
