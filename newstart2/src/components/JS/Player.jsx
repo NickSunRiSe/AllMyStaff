09576acd9caee211d61bd5846f9bbdc2d41d6098
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-04-25 21:26:05 +0400

    Изучение реакта 04.25.2022

--- newstart2/src/components/JS/Player.jsx
+++ newstart2/src/components/JS/Player.jsx
@@ -103,7 +103,16 @@ const Player = () => {
             <li class="jp-player-list-item" data-jp-title="Истень - Вьюга" data-jp-mp3="../sounds/Ustal.mp3"></li>
             </ul>
             </div>
-
+            <div class="tab-pane fade show active" id="tabs-1-16">
+            <ul class="jp-player-list">
+            <li class="jp-player-list-item" data-jp-title="Истень - Вьюга" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            </ul>
+            </div>
+            <div class="tab-pane fade show active" id="tabs-1-17">
+            <ul class="jp-player-list">
+            <li class="jp-player-list-item" data-jp-title="Истень - Вьюга" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            </ul>
+            </div>
 
 
 
