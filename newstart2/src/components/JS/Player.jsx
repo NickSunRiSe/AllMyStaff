d0a72900e5c3d80d5337b622eeb3e007ccb820b5
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-04-30 13:46:52 +0400

    Изучение реакта 04.30.2022

--- newstart2/src/components/JS/Player.jsx
+++ newstart2/src/components/JS/Player.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Ustal from "../sounds/Ustal.mp3"
+import Reactaudio from 'react-audio-player'
 
 const Player = () => {
   return (
@@ -26,91 +27,162 @@ const Player = () => {
                     <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-1-17" data-toggle="tab">Грустные песни о успехах моей жизни</a></li>
                   </ul>
                 </div>
-
+                <Reactaudio
+             src={Ustal}
+               controls
+            />
                 <div class="tab-content">
                 <div class="tab-pane fade show active" id="tabs-1-1">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Вьюга" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Вьюга"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
             <div class="tab-pane fade show active" id="tabs-1-2">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Мы" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Мы"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
             <div class="tab-pane fade show active" id="tabs-1-3">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Этот день 24" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Этот день 24"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
             <div class="tab-pane fade show active" id="tabs-1-4">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Этот день (Гитара)" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Этот день (Гитара)"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
             <div class="tab-pane fade show active" id="tabs-1-5">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Свет" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Свет"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
             <div class="tab-pane fade show active" id="tabs-1-6">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Миг" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Миг"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
             <div class="tab-pane fade show active" id="tabs-1-7">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Дождь" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Дождь"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
             <div class="tab-pane fade show active" id="tabs-1-8">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Солнечный диск" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Солнечный диск"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
             <div class="tab-pane fade show active" id="tabs-1-9">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Небо" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Небо"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
             <div class="tab-pane fade show active" id="tabs-1-10">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Глаза" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Глаза"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
             <div class="tab-pane fade show active" id="tabs-1-11">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Место (Укулеле)" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Место (Укулеле)"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
             <div class="tab-pane fade show active" id="tabs-1-12">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Место (Гитара)" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Место (Гитара)"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div> 
             <div class="tab-pane fade show active" id="tabs-1-13">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Когда" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Когда"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div> 
             <div class="tab-pane fade show active" id="tabs-1-14">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Страшный сон" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Страшный сон"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div> 
             <div class="tab-pane fade show active" id="tabs-1-15">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Сломанный телефон" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Сломанный телефон"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
             <div class="tab-pane fade show active" id="tabs-1-16">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Причины искусства" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Причины искусства"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
             <div class="tab-pane fade show active" id="tabs-1-17">
             <ul class="jp-player-list">
-            <li class="jp-player-list-item" data-jp-title="Истень - Грустные песни о успехах моей жизни" data-jp-mp3="../sounds/Ustal.mp3"></li>
+            <li class="jp-player-list-item" data-jp-title="Истень - Грустные песни о успехах моей жизни"></li>
+            <Reactaudio
+             src={Ustal}
+               controls
+            />
             </ul>
             </div>
 
