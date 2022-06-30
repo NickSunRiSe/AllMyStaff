1340b79708b8d534321ea568795d4ee2c3e4d0ad
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-06-30 19:20:16 +0400

    Portfolio react

--- Портфолио/ReactPortfolio/reactport/src/Components/Header.jsx
+++ Портфолио/ReactPortfolio/reactport/src/Components/Header.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {Link } from "react-router-dom";
 import Player from "./Player"
 
 const Header = () => {
   return (
     <div>
         <Player/>
-       
     </div>
   );
 };
--- Портфолио/ReactPortfolio/reactport/src/Components/Pages/Nothing.jsx
+++ Портфолио/ReactPortfolio/reactport/src/Components/Pages/Nothing.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import Header from "../Header";
+import End from "../End";
 
 const Nothing = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="con">
+      <Header />
+      <div className="nothing">
+        Данная страница не содержит какого либо контента.
+      </div>
+      <End />
+    </div>
+  );
 };
 
 export default Nothing;
