3e006137478ec16539c654df789a75046fbadadd
Nick <bbbb.bbbb.2001.nik@gmail.com>
2022-07-20 19:11:42 +0400

    процесс

--- Портфолио/isten/src/App.js
+++ Портфолио/isten/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import Nav__menu from "./Elements/Nav__menu"
   
-function App() {
+function App() { 
   return ( 
     <Router>
         <Nav__menu/> 
